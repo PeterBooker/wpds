2254f9954a3fa75a6f352508f858de1d78765457
Peter Booker <mail@peterbooker.com>
2018-04-04 09:42:58 +0100

    more detailed/specific client

--- internal/pkg/slurper/client.go
+++ internal/pkg/slurper/client.go
@@ -14,17 +14,17 @@ import (
 func NewClient(timeout int, max int) *http.Client {
 
 	var netTransport = &http.Transport{
+		Proxy: http.ProxyFromEnvironment,
 		DialContext: (&net.Dialer{
 			Timeout:   30 * time.Second,
 			KeepAlive: 30 * time.Second,
+			DualStack: true,
 		}).DialContext,
-		IdleConnTimeout:       45 * time.Second,
+		MaxIdleConns:          100,
+		IdleConnTimeout:       90 * time.Second,
 		TLSHandshakeTimeout:   10 * time.Second,
 		ExpectContinueTimeout: 1 * time.Second,
-		MaxIdleConns:          max,
-		// Is this more efficient?
-		MaxIdleConnsPerHost: runtime.GOMAXPROCS(0) + 1,
-		//MaxIdleConnsPerHost:   max,
+		MaxIdleConnsPerHost:   runtime.GOMAXPROCS(0) + 1,
 	}
 
 	var netClient = &http.Client{
