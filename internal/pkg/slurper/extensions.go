3e2415656ede7e7aeecd423615d4d60ce786510a
Peter Booker <mail@peterbooker.com>
2018-04-07 16:21:27 +0100

    small restructure added ability to download readme files only

--- internal/pkg/slurper/extensions.go
+++ internal/pkg/slurper/extensions.go
@@ -5,6 +5,7 @@ import (
 	"io/ioutil"
 	"log"
 	"net/http"
+	"os"
 	"path/filepath"
 	"sync"
 
@@ -57,60 +58,44 @@ func fetchExtensions(extensions []string, ctx *context.Context) error {
 
 }
 
+// getExtension decides which extension data to fetch.
 func getExtension(name string, ctx *context.Context, wg *sync.WaitGroup) {
 
-	client := NewClient(120, ctx.ConcurrentActions)
+	var file []byte
+	var err error
+	var size uint64
 
-	var eURL string
+	switch ctx.FileType {
+	case "all":
 
-	switch ctx.ExtensionType {
-	case "plugins":
-		eURL = utils.EncodeURL(fmt.Sprintf(wpPluginDownloadURL, name))
-	case "themes":
-		eURL = utils.EncodeURL(fmt.Sprintf(wpThemeDownloadURL, name))
-	}
-
-	req, err := http.NewRequest("GET", eURL, nil)
-	if err != nil {
-		log.Println(err)
-	}
-
-	// Dynamically set User-Agent from config
-	req.Header.Set("User-Agent", config.GetName()+"/"+config.GetVersion())
-
-	resp, err := client.Do(req)
-	if err != nil {
-		extensionFailure(name, ctx)
-		return
-	}
-
-	if resp.StatusCode != 200 {
-
-		// Code 404 is acceptable, means the plugin/theme is no longer available.
-		if resp.StatusCode == 404 {
-			ctx.Stats.IncrementTotalExtensionsClosed()
+		file, err = getExtensionZip(name, ctx)
+		if err != nil {
+			extensionFailure(name, ctx)
 			return
 		}
 
-		log.Printf("Downloading the extension '%s' failed. Response code: %d\n", name, resp.StatusCode)
+		// TODO: Check return values.
+		size, err = ExtractZip(file, int64(len(file)), name, ctx)
+		if err != nil {
+			extensionFailure(name, ctx)
+			return
+		}
 
-		extensionFailure(name, ctx)
-		return
+	case "readme":
 
-	}
+		file, err = getExtensionReadme(name, ctx)
+		if err != nil {
+			extensionFailure(name, ctx)
+			return
+		}
 
-	content, err := ioutil.ReadAll(resp.Body)
-	if err != nil {
-		extensionFailure(name, ctx)
-		return
-	}
+		// TODO: Check return value.
+		size, err = writeReadme(file, name, ctx)
+		if err != nil {
+			extensionFailure(name, ctx)
+			return
+		}
 
-	resp.Body.Close()
-
-	size, err := ExtractZip(content, resp.ContentLength, name, ctx)
-	if err != nil {
-		extensionFailure(name, ctx)
-		return
 	}
 
 	ctx.Stats.IncrementTotalExtensions()
@@ -118,39 +103,13 @@ func getExtension(name string, ctx *context.Context, wg *sync.WaitGroup) {
 
 }
 
-// GetExtension decides which extension data to fetch.
-func GetExtension(name string, ctx *context.Context) {
+// getExtensionZip gets the extension archive.
+func getExtensionZip(name string, ctx *context.Context) ([]byte, error) {
 
-	var file []byte
-	var err error
-
-	switch ctx.FileType {
-	case "all":
-
-		file, err = GetExtensionZip(name, ctx)
-		if err != nil {
-			// TODO: Handle Error
-		}
-
-		ExtractZip(file, int64(len(file)), "", ctx)
-
-	case "readme":
-
-		file, err = GetExtensionReadme(name, ctx)
-		if err != nil {
-			// TODO: Handle Error
-		}
-
-		//utils.WriteReadmeFile(file, name)
-
-	}
-
-}
-
-// GetExtensionZip gets the extension archive.
-func GetExtensionZip(name string, ctx *context.Context) ([]byte, error) {
+	client := NewClient(120, ctx.ConcurrentActions)
 
 	var URL string
+	var content []byte
 
 	switch ctx.ExtensionType {
 	case "plugins":
@@ -159,20 +118,51 @@ func GetExtensionZip(name string, ctx *context.Context) ([]byte, error) {
 		URL = utils.EncodeURL(fmt.Sprintf(wpThemeDownloadURL, name))
 	}
 
-	response, err := NewRequest(URL, 60, ctx.ConcurrentActions)
+	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
-		// TODO: Handle Failed Download
-		return response, err
+		log.Println(err)
+		return content, err
 	}
 
-	return response, nil
+	// Dynamically set User-Agent from config
+	req.Header.Set("User-Agent", config.GetName()+"/"+config.GetVersion())
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return content, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+
+		// Code 404 is acceptable, means the plugin/theme is no longer available.
+		if resp.StatusCode == 404 {
+			ctx.Stats.IncrementTotalExtensionsClosed()
+			return content, nil
+		}
+
+		log.Printf("Downloading the extension '%s' failed. Response code: %d\n", name, resp.StatusCode)
+
+		return content, err
+
+	}
+
+	content, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return content, err
+	}
+
+	return content, err
 
 }
 
-// GetExtensionReadme gets the extension readme.
-func GetExtensionReadme(name string, ctx *context.Context) ([]byte, error) {
+// getExtensionReadme gets the extension readme.
+func getExtensionReadme(name string, ctx *context.Context) ([]byte, error) {
+
+	client := NewClient(120, ctx.ConcurrentActions)
 
 	var URL string
+	var content []byte
 
 	switch ctx.ExtensionType {
 	case "plugins":
@@ -181,12 +171,85 @@ func GetExtensionReadme(name string, ctx *context.Context) ([]byte, error) {
 		URL = utils.EncodeURL(fmt.Sprintf(wpThemeReadmeURL, name))
 	}
 
-	response, err := NewRequest(URL, 60, ctx.ConcurrentActions)
+	req, err := http.NewRequest("GET", URL, nil)
 	if err != nil {
-		// TODO: Handle Failed Download
-		return response, err
+		log.Println(err)
+		return content, err
 	}
 
-	return response, nil
+	// Dynamically set User-Agent from config
+	req.Header.Set("User-Agent", config.GetName()+"/"+config.GetVersion())
+
+	resp, err := client.Do(req)
+	if err != nil {
+		return content, err
+	}
+	defer resp.Body.Close()
+
+	if resp.StatusCode != 200 {
+
+		// Code 404 is acceptable, means the plugin/theme is no longer available.
+		if resp.StatusCode == 404 {
+			ctx.Stats.IncrementTotalExtensionsClosed()
+			return content, nil
+		}
+
+		log.Printf("Downloading the extension '%s' failed. Response code: %d\n", name, resp.StatusCode)
+
+		return content, err
+
+	}
+
+	content, err = ioutil.ReadAll(resp.Body)
+	if err != nil {
+		return content, err
+	}
+
+	return content, err
+
+}
+
+// writeReadme writes the readme file to disk.
+func writeReadme(content []byte, name string, ctx *context.Context) (uint64, error) {
+
+	base := filepath.Join(wd, ctx.ExtensionType, name)
+
+	if utils.DirExists(base) {
+		err := utils.RemoveDir(base)
+		if err != nil {
+			log.Printf("Cannot delete extension folder: %s%s\n", ctx.ExtensionType, base)
+		}
+	}
+
+	// Create base dir
+	utils.CreateDir(base)
+
+	path := filepath.Join(wd, ctx.ExtensionType, name, "readme.txt")
+
+	if _, err := os.Stat(path); os.IsNotExist(err) {
+
+		f, err := os.Create(path)
+		defer f.Close()
+		if err != nil {
+			return 0, err
+		}
+
+	}
+
+	f, err := os.OpenFile(path, os.O_RDWR, 0777)
+	if err != nil {
+		return 0, err
+	}
+
+	defer f.Close()
+
+	_, err = f.Write(content)
+	if err != nil {
+		return 0, err
+	}
+
+	fi, _ := f.Stat()
+
+	return uint64(fi.Size()), nil
 
 }
