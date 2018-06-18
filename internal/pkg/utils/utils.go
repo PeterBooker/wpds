561481e182f6c49b14de7762a05ef6845ea4ed18
Peter Booker <mail@peterbooker.com>
2018-06-18 11:21:44 +0100

    add improved err checking functions for io Close

--- internal/pkg/utils/utils.go
+++ internal/pkg/utils/utils.go
@@ -1,6 +1,8 @@
 package utils
 
 import (
+	"io"
+	"io/ioutil"
 	"net/url"
 )
 
@@ -14,4 +16,25 @@ func EncodeURL(rawURL string) string {
 
 	return URL
 
+}
+
+// checkClose is used to check the return from Close in a defer statement.
+func checkClose(c io.Closer, err *error) {
+	cerr := c.Close()
+	if *err == nil {
+		*err = cerr
+	}
+}
+
+// drainAndClose discards all data from rd and closes it.
+func drainAndClose(rd io.ReadCloser, err *error) {
+	if rd == nil {
+		return
+	}
+
+	_, _ = io.Copy(ioutil.Discard, rd)
+	cerr := rd.Close()
+	if err != nil && *err == nil {
+		*err = cerr
+	}
 }
